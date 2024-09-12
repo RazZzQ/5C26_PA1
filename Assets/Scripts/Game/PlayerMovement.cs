7b0e8b9f2f8700e15729141c853413d6e21a62ff
RazZzQ <roswell.quispe@tecsup.edu.pe>
2024-09-12 09:12:00 -0500

    Initial commit

---
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D myRB;
+    private float limitSuperior;
+    private float limitInferior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        SetMinMax();
+    }
+
+    void SetMinMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        limitInferior = -bounds.y;
+        limitSuperior = bounds.y;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Candy")
+        {
+            
+        }
+
+        if(other.tag == "Enemy")
+        {
+            
+        }
+    }
+
+    public void OnMovement()
+    {
+
+    }
+}
